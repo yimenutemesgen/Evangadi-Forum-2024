73f75696a220f3236c47d5ca3d6e6ce380947eb6
mymom0937 <mymom0937@gmail.com>
2024-08-06 22:18:57 +0100

    fetch and post answer

---
+++ app.js
@@ -0,0 +1,47 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = 5000;
+const cors = require("cors");
+
+app.use(cors());
+// db Connection
+const dbConnection = require("./db/dbConfig.js");
+
+// authentication middleware file
+const authMiddleware = require("./middleware/authMiddleware.js");
+
+// json middleware to extract json data
+app.use(express.json());
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+//*********user routes middleware file & user routes middleware*****************//
+const userRoutes = require("./routes/userRoutes.js");
+app.use("/api/user", userRoutes);
+//*********user routes middleware file & user routes middleware*****************//
+
+//*********question routes middleware file & question routes middleware*****************//
+// const questionRoutes = require("./routes/questionRoutes.js");
+// app.use("/api/question", authMiddleware, questionRoutes);
+
+//*******question routes middleware file & question routes middleware***********//
+
+
+//*********answer routes middleware file & answer routes middleware*****************//
+const answerRoutes = require("./routes/answerRoutes.js");
+app.use("/api/answer", authMiddleware, answerRoutes);
+//*********answer routes middleware file & answer routes middleware***********//
+
+async function start() {
+  try {
+    const result = await dbConnection.execute("select 'test' ");
+    await app.listen(PORT);
+    console.log("Database connection established");
+    console.log(`listening on ${PORT}`);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+start();
---
+++ routes/userRoutes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router()
+
+//  authentication middleware
+const authMiddleware = require("../middleware/authMiddleware.js")
+
+//  User Controller
+const {
+  register,
+  login,
+  checkUser,
+  resetPassword,
+} = require("../controller/userController.js");
+// Register routes
+router.post("/register", register);
+
+// Login User
+router.post("/login", login);
+
+// Check User
+router.get("/check", authMiddleware, checkUser);
+
+// Reset Password
+router.post("/password-reset", resetPassword);
+
+module.exports = router;
